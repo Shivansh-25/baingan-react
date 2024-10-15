db9d675c5172e82dac7669a1e341265bc8a64ced
Ankur <ac7351318355@gmail.com>
2024-10-15 18:53:18 +0530

    changes to database-config

--- backend/database-config/database-config.js
+++ backend/database-config/database-config.js
@@ -3,31 +3,10 @@ import serviceAccount from '../baingan-79603-firebase-adminsdk-91hpd-c23fb5e65c.
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://baingan-79603.firebaseio.com'
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 const db = admin.firestore();
 const auth = admin.auth();
 
 export {admin, db, auth};
-
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB0aK5RNhBkBqBtTkT3XY0TbAUW3RASrFk",
-//   authDomain: "baingan-79603.firebaseapp.com",
-//   projectId: "baingan-79603",
-//   storageBucket: "baingan-79603.appspot.com",
-//   messagingSenderId: "958113275231",
-//   appId: "1:958113275231:web:af6291868c8699a2ba2aea"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// const auth = getAuth(app);
-// const db = getFirestore(app);
-
-// export { auth, db };
